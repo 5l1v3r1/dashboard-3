0d5eefb9060496e77ca10936cb93bdb92418d492
Ben Lowry <benlowry@outlook.com>
2019-08-07 19:35:28 -0600

    Documentation has own server now

--- start-screenshots.sh
+++
@@ -1,9 +0,0 @@
-NODE_ENV=development \
-PAGE_SIZE=3 \
-DASHBOARD_SERVER="http://localhost:8001" \
-PORT=8001 \
-STORAGE_PATH=/tmp/test-data \
-REQUIRE_PROFILE_EMAIL=true \
-REQUIRE_PROFILE_NAME=true \
-ID_LENGTH=8 \
-node main.js
