ee8c096b44fc95af3d228fd816046b4538fa910e
Ben Lowry <benlowry@outlook.com>
2019-09-07 17:30:44 -0600

    Dependecy update script

---
+++ update-dependencies.sh
@@ -0,0 +1,9 @@
+npm update bcryptjs || exit 1
+npm update multiparty || exit 1
+npm update server-html || exit 1
+npm update uglify-js || exit 1
+npm update puppeteer || exit 1
+npm shrinkwrap --dev || exit 1
+git add npm-shrinkwrap.json || exit 1
+git commit -m "Updated dependencies" || exit 1
+git push origin master || exit 1
