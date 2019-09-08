7308b7ae25cfacd3b0c7f8848b439f717acc2871
Ben Lowry <benlowry@outlook.com>
2019-09-08 09:46:43 -0600

    Commit package.json changes

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -5,5 +5,6 @@ npm update uglify-js || exit 1
 npm update puppeteer || exit 1
 npm shrinkwrap --dev || exit 1
 git add npm-shrinkwrap.json || exit 1
+git add package.json || exit 1
 git commit -m "Updated dependencies" || exit 1
 git push origin master || exit 1
