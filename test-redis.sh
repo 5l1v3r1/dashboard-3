9b4a1b35650737939e544e90bab75625c41d6a6e
benlowry <benlowry@outlook.com>
2019-11-21 20:58:48 +0000

    Updated dependency installs

--- test-redis.sh
+++ test-redis.sh
@@ -1,8 +1,16 @@
+if [ ! -d node_modules/puppeteer ] || [ ! -d node_modules/@userdashboard/storage-redis ]; then
+  npm install puppeteer @userdashboard/storage-redis --no-save
+fi
+
 NODE_ENV=testing \
-FAST_START=true \
-PAGE_SIZE=3 \
-DASHBOARD_SERVER="http://localhost:9002" \
-PORT=9002 \
 STORAGE_ENGINE="@userdashboard/storage-redis" \
 REDIS_URL=redis://localhost:6379 \
+FAST_START=true \
+DASHBOARD_SERVER="http://localhost:9007" \
+PORT=9007 \
+STORAGE_PATH=/tmp/test-data \
+ENCRYPTION_SECRET=12345678901234567890123456789012 \
+ENCRYPTION_SECRET_IV=1234123412341234 \
+GENERATE_SITEMAP_TXT=false \
+GENERATE_API_TXT=false \
 npm test
