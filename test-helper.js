0017d9b1168e1578d270dd33a3ebc2397079bfc8
benlowry <benlowry@outlook.com>
2020-01-29 20:53:45 +0000

    Testing 'real' user agent for Stripe modules

--- test-helper.js
+++ test-helper.js
@@ -373,7 +373,7 @@ const proxy = util.promisify((method, path, req, callback) => {
     timeout: 180000,
     method: method.toUpperCase(),
     headers: {
-      'user-agent': 'integration tests'
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0'
     }
   }
   let postData
