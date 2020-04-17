cf5f697977c9189a83e970116e53190cff68ac37
benlowry <benlowry@outlook.com>
2020-04-17 14:29:26 +0000

    Don't enforce minimum username/password lengths in case of configuration change

--- src/www/account/verify.test.js
+++ src/www/account/verify.test.js
@@ -32,22 +32,6 @@ describe('/account/verify', () => {
       assert.strictEqual(message.attr.template, 'invalid-username')
     })
 
-    it('should enforce username length', async () => {
-      const user = await TestHelper.createUser()
-      const req = TestHelper.createRequest('/account/verify?return-url=/redirecting')
-      req.account = user.account
-      req.session = user.session
-      req.body = {
-        username: '1',
-        password: '123456789123'
-      }
-      global.minimumUsernameLength = 100
-      const result = await req.post()
-      const doc = TestHelper.extractDoc(result.html)
-      const message = doc.getElementById('message-container').child[0]
-      assert.strictEqual(message.attr.template, 'invalid-username-length')
-    })
-
     it('should reject missing password', async () => {
       const user = await TestHelper.createUser()
       const req = TestHelper.createRequest('/account/verify?return-url=/redirecting')
@@ -76,22 +60,6 @@ describe('/account/verify', () => {
       const doc = TestHelper.extractDoc(result.html)
       const message = doc.getElementById('message-container').child[0]
       assert.strictEqual(message.attr.template, 'invalid-password')
-    })
-
-    it('should enforce password length', async () => {
-      const user = await TestHelper.createUser()
-      const req = TestHelper.createRequest('/account/verify?return-url=/redirecting')
-      req.account = user.account
-      req.session = user.session
-      req.body = {
-        username: '1234567890123',
-        password: '1'
-      }
-      global.minimumPasswordLength = 100
-      const result = await req.post()
-      const doc = TestHelper.extractDoc(result.html)
-      const message = doc.getElementById('message-container').child[0]
-      assert.strictEqual(message.attr.template, 'invalid-password-length')
     })
 
     it('should mark session as verified', async () => {
