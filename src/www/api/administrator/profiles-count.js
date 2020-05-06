5e43806baf1e293c90d133e697d0e0fbfe1dbe78
benlowry <benlowry@outlook.com>
2020-05-06 07:23:43 -0600

    Fix null reference

--- src/www/api/administrator/profiles-count.js
+++ src/www/api/administrator/profiles-count.js
@@ -2,6 +2,7 @@ const dashboard = require('../../../../index.js')
 
 module.exports = {
   get: async (req) => {
+    req.query = req.query || {}
     let index
     if (req.query.accountid) {
       index = `${req.appid}/account/profiles/${req.query.accountid}`
