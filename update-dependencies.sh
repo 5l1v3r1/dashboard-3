a45deb65cf03b21bdcc252c085a1e46064096cc9
Ben Lowry <benlowry@pop-os.localdomain>
2020-05-27 14:30:36 -0600

    Cleaning up scripts

--- update-dependencies.sh
+++
@@ -1,9 +0,0 @@
-npm update bcryptjs || exit 1
-npm update multiparty || exit 1
-npm update server-html || exit 1
-npm update uglify-js || exit 1
-npm shrinkwrap --dev || exit 1
-git add npm-shrinkwrap.json || exit 1
-git add package.json || exit 1
-git commit -m "Updated dependencies" || exit 1
-git push origin master || exit 1
